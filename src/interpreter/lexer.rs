076532bc1c6528b7570553652221a7bbd853dfea
Hyeokjune Jeon <fantajeon@gmail.com>
2022-04-13 07:10:17 +0900

    refactoring, lexer()

--- src/interpreter/lexer.rs
+++ src/interpreter/lexer.rs
@@ -15,34 +15,31 @@ pub fn lexer(express: &str) -> Result<VecDeque<Token>, String> {
         |(?P<int>[+\-]?[0-9]+)          # digital
         |(?P<whitespace>\s+)    # whitespace
         ").unwrap();
-    let mut toks: VecDeque<Token> = VecDeque::new();
-    for cap in re.captures_iter(express) {
-        let tok = re.capture_names()
-            .flatten()
-            .filter_map(|n| Some((n,cap.name(n)?.as_str().to_string())) )
-            .take(1).next().unwrap();
-        let t: Option<Token> = match tok.0 {
-            "int" => Some( Token::new( Kind::IntNumber(tok.1.parse::<i32>().unwrap()), &tok.1) ),
-            "float" => Some( Token::new( Kind::FloatNumber(tok.1.parse::<f32>().unwrap()), &tok.1) ),
-            "letter" => if tok.1 == "fn" {
-                Some( Token::new( Kind::Keyword(tok.1.clone()), &tok.1) )
-            } else {
-                Some( Token::new( Kind::Letter(tok.1.clone()), &tok.1) )
-            },
-            "special" => match tok.1.as_str() {
-                "=" => Some( Token::new( Kind::ASSIGN, &tok.1 ) ),
-                "(" => Some( Token::new( Kind::LPAREN, &tok.1 ) ),
-                ")" => Some( Token::new( Kind::RPAREN, &tok.1 ) ),
-                _ => return Err(format!("unkonwn speical token:{}", tok.1)),
-            },
-            "fn_op" => Some( Token::new( Kind::FNOP, &tok.1 ) ),
-            "ops" => Some( Token::new( Kind::Op(tok.1.clone()), &tok.1) ),
-            _ => None,
-        };
-        if t.is_some() {
-            toks.push_back(t.unwrap());
-        }
-    }
+    let mut toks: VecDeque<Token> = re.captures_iter(express).filter_map(|cap| -> Option<_> {
+            let tok = re.capture_names()
+                .flatten()
+                .filter_map(|n| Some((n,cap.name(n)?.as_str().to_string())) )
+                .take(1).next().unwrap();
+            let t: Option<Token> = match tok.0 {
+                "int" => Some( Token::new( Kind::IntNumber(tok.1.parse::<i32>().unwrap()), &tok.1) ),
+                "float" => Some( Token::new( Kind::FloatNumber(tok.1.parse::<f32>().unwrap()), &tok.1) ),
+                "letter" => if tok.1 == "fn" {
+                    Some( Token::new( Kind::Keyword(tok.1.clone()), &tok.1) )
+                } else {
+                    Some( Token::new( Kind::Letter(tok.1.clone()), &tok.1) )
+                },
+                "special" => match tok.1.as_str() {
+                    "=" => Some( Token::new( Kind::ASSIGN, &tok.1 ) ),
+                    "(" => Some( Token::new( Kind::LPAREN, &tok.1 ) ),
+                    ")" => Some( Token::new( Kind::RPAREN, &tok.1 ) ),
+                    _ => unreachable!("please check your special character!"),
+                },
+                "fn_op" => Some( Token::new( Kind::FNOP, &tok.1 ) ),
+                "ops" => Some( Token::new( Kind::Op(tok.1.clone()), &tok.1) ),
+                _ => None,
+            };
+            return t;
+        }).collect();
 
     toks.push_back(Token::new_eof());
 
